3aaedfbd49cd2ac6f03ca374f8edc527afa07702
mila-hristova <eaa23mihr@students.eaaa.dk>
2024-10-22 10:18:40 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
+
 export default function HomePage() {
+    useEffect(() => {
+        const url =  
+        "https://react-crud-10294-default-rtdb.firebaseio.com//posts.json";
+    }, []);
+    
     return (
         <section className="page">
             <h1>Home Page</h1>
             <p>Home is where the heart is 💛</p>
             <p>Oh My, sounds like a bad movie!</p>
         </section>
-    );
+    );
 }
